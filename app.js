f4441b32f2da1109e0abb0e77e90f9a4e0cc5089
GeneZijlboot <gene.cyberzijlmans@gmail.com>
2023-12-12 13:09:20 +0100

    succesfully connected to the mongodv database

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
-const express = require("express");
+const express = require('express');
+const { MongoClient } = require('mongodb');
 const app = express();
-
 app.set('view engine', 'ejs');
-const port = 3000;
+const PORT = 3000;
 
+const mongoDB_URI = "mongodb+srv://Gene:Admin@genedb.q7uhupi.mongodb.net/GameGenres?retryWrites=true&w=majority";
 
-//set up mongoose connection
-const mongoose = require("mongoose");
-mongoose.set("strictQuery", false);
-const mongoDB = "mongodb+srv://Gene:Admin@genedb.q7uhupi.mongodb.net/Inventory_Application?retryWrites=true&w=majority";
+//async function to connect to db
 
-main().catch((err) => console.log(err));
-async function main() {
-    await mongoose.connect(mongoDB)
-    console.log("connected");
+async function connectToMongoDB(){
+    const client = new MongoClient(mongoDB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-    // Assuming you have a model named 'Item' for your collection
-    const Birds = mongoose.model('Birds', { name: String });
-
-    // Example: Log all items in the 'items' collection
-    const birds = await Birds.find({});
-    console.log("Items in the database:", Birds);
+    try {
+        await client.connect();
+        console.log('Connected to MongoDB database!');
+        return client
+    } catch(error) {
+        console.error('Error connecting to MongoDB:', error);
+        throw error;
+    }
 }
+
+//paths --> place them in routes folder soon!
 
 app.get("/", (req, res, next) => {
     console.log("Home Page entered");
@@ -43,6 +43,14 @@ app.get("/Games", (req, res, next) => {
     res.render('Games');
 });
 
-app.listen(port, () => {
-    console.log(`Server running on: http://localhost:${port}`);
+//run the server on port 3000
+
+app.listen(PORT, async () => {
+    try {
+        console.log(`Server running on: http://localhost:${PORT}`);
+        const client = await connectToMongoDB();
+    } catch(error){
+        console.log(error);
+        process.exit(1);
+    }
 });
