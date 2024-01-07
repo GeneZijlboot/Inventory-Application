257e9dcc89c18a854b442a96463b9b29b631bceb
GeneZijlboot <gene.cyberzijlmans@gmail.com>
2024-01-07 18:20:40 +0100

    can now delete Specific Games

--- routes/GenreRoutes.js
+++ routes/GenreRoutes.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 const { ObjectId } = require('mongodb');
 const { connectToMongoDB, insertDocument } = require('../dbHandlings/DB_Connect');
 const { createCollection } = require('../dbHandlings/createCollection');
 const { createDocument } = require('../dbHandlings/createDocument');
 
 router.use(bodyParser.urlencoded({ extended: true }));
+router.use(methodOverride('_method'));
 
 router.get("/AddGenre", (req, res, next) => {
     res.render('AddGenre');
@@ -16,6 +18,8 @@ router.get("/AddGame/:collectionName", (req, res, next) => {
     const collectionName = req.params.collectionName;
     res.render('AddGame', { collectionName });
 });
+
+//Display Genre, Game, Game Detaile
 
 router.get("/:collectionName/:documentId", async (req, res, next) => {
     const collectionName = req.params.collectionName;
@@ -60,6 +64,8 @@ router.get("/", async (req, res, next) => {
     }
 });
 
+//Creating Genre and Game
+
 router.post('/:collectionName', async (req, res) => {
     const collectionName = req.params.collectionName;
     const document = {
@@ -91,6 +97,25 @@ router.post('/', async (req, res) => {
         await createCollection(database, genre);
         await client.close();
         res.redirect('/Genres');
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+//Delete Genre and Game
+
+router.delete("/:collectionName/:documentId", async (req, res, next) => {
+    const collectionName = req.params.collectionName;
+    const documentId = req.params.documentId;
+
+    try {
+        const client = await connectToMongoDB();
+        const database = client.db('GameGenres');
+        const collection = database.collection(collectionName);
+        await collection.deleteOne({ _id: new ObjectId(documentId) });
+
+        res.redirect(`/Genres/${collectionName}`);
     } catch (error) {
         console.error('Error:', error);
         res.status(500).send('Internal Server Error');
