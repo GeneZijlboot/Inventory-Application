c3e25c6bc5ef815fd9e3a72964940df7c2734cba
GeneZijlboot <gene.cyberzijlmans@gmail.com>
2024-01-07 18:54:30 +0100

    deleting file

--- addGame.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const newGameSchema = new mongoose.Schema({
-    title: String,
-    description: String,
-    price: String
-})
-
-module.exports = mongoose.model("newGame", newGameSchema);
